381e7781e64f1b7eee1d36321d982a9aa7c822c0
mrbiggleswirth <32403340+mrbiggleswirth@users.noreply.github.com>
2025-03-04 18:14:00 +0100

    Video 4 of 4.

--- src/main/java/com/example/JwtIntro/AuthController.java
+++ src/main/java/com/example/JwtIntro/AuthController.java
@@ -3,51 +3,58 @@ package com.example.JwtIntro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private JwtUtil jwtUtil;
-
-    private final String DEMO_USER = "david";
-    private final String DEMO_PASSWORD = "1234567";
-
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Map<String, String> user) {
         String username = user.get("username");
         String password = user.get("password");
 
-        if(DEMO_USER.equals(username) && DEMO_PASSWORD.equals(password)) {
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+
+        if(optionalUser.isPresent() && passwordEncoder.matches(password, optionalUser.get().getPassword()) ){
+
             String token = jwtUtil.generateToken(username);
             return ResponseEntity.ok(Collections.singletonMap("token", token));
+
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+            return ResponseEntity.status(401).body("Invalid username or passowrd");
+        }
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> verifyToken(@RequestHeader("Authorization") String authHeader ){
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ") ){
+            return ResponseEntity.status(401).body("Missing or invalid token");
+        }
+
+        String token = authHeader.substring(7);
+        String username = jwtUtil.validateToken(token);
+
+        if(username != null ){
+            return ResponseEntity.ok(Collections.singletonMap("user", username));
+        } else {
+            return ResponseEntity.status(401).body("Invalid token");
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
